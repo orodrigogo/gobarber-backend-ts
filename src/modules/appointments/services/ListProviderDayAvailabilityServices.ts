ecbea15f3af0859971407e2cf8e083dc740eda85
rodrigo <rodrigo.rgtic@gmail.com>
2020-06-30 17:05:47 -0300

    feat: remove old hours from list

--- src/modules/appointments/services/ListProviderDayAvailabilityServices.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityServices.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -45,13 +45,18 @@ class ListProviderDayAvailabilityServices {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
+
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate), // removendo da listagem as horas que já passaram.
       };
     });
 
