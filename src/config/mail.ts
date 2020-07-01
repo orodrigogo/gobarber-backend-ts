0980eecad2fe4cfa922f2ca8c6bc6dd2d1005d96
rodrigo <rodrigo.rgtic@gmail.com>
2020-07-01 20:55:00 -0300

    feat: send email with aws ses

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'rodrigo@gostudent.com.br',
+      name: 'Rodrigo',
+    },
+  },
+} as IMailConfig;
