666f10a0383f6e8b89d83362db6dd98be1b8a71f
rodrigo <rodrigo.rgtic@gmail.com>
2020-07-01 21:17:49 -0300

    refactor: inversion dependency

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,37 +1,4 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-import ICacheProvider from './CacheProvider/models/ICacheProvider';
-import RedisCacheProvider from './CacheProvider/implementations/RedisCacheProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
-
-container.registerInstance<ICacheProvider>(
-  'CacheProvider',
-  container.resolve(RedisCacheProvider),
-);
+import './MailProvider';
+import './MailTemplateProvider';
+import './StorageProvider';
+import './CacheProvider';
