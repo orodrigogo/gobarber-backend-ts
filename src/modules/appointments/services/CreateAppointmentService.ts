81f8f5d03829cb2a72deb342a1483372ffffe088
rodrigo <rodrigo.rgtic@gmail.com>
2020-07-04 09:23:18 -0300

    refactor: appointments by user id

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -50,6 +50,7 @@ class CreateAppointmentService {
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if (findAppointmentInSameDate) {
